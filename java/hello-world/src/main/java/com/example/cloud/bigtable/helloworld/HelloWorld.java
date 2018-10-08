668e470e03560a2a76455fd1ce6c3cefa3cf08dd
DhineshFuelX <40577209+DhineshFuelX@users.noreply.github.com>
2018-10-07 20:57:01 -0700

    Update HelloWorld.java

--- java/hello-world/src/main/java/com/example/cloud/bigtable/helloworld/HelloWorld.java
+++ java/hello-world/src/main/java/com/example/cloud/bigtable/helloworld/HelloWorld.java
@@ -114,13 +114,20 @@ public class HelloWorld {
 
       // [START scanning_all_rows]
       // Now scan across all rows.
-      byte[] startScan = Bytes.add(Bytes.toBytes(START), Bytes.toBytes("#"), Bytes.toBytes(BID));
+      //byte[] startScan = Bytes.add(Bytes.toBytes(START), Bytes.toBytes("#"), Bytes.toBytes(BID));
 
-      byte[] endScan = Bytes.add(Bytes.toBytes(END), Bytes.toBytes("#"), Bytes.toBytes(BID));
+      //byte[] endScan = Bytes.add(Bytes.toBytes(END), Bytes.toBytes("#"), Bytes.toBytes(BID));
+      List<Filter> filters = new ArrayList<>();
+      //Filter filter2 = new SingleColumnValueFilter(Bytes.toBytes("info"), Bytes.toBytes("bid"),CompareFilter.CompareOp.EQUAL,Bytes.toBytes(bid));
+      Filter filter2 = new RowFilter(CompareFilter.CompareOp.EQUAL,new RegexStringComparator(".*#"+bid+"#.*"));
+      filters.add(filter2);
       //byte[] startScan = Bytes.add(Bytes.toBytes(START+"#"+BID));
 
 //       byte[] endScan = Bytes.add(Bytes.toBytes(END+"#"+BID));
-      Scan scan = new Scan(startScan, endScan);
+      //Scan scan = new Scan(startScan, endScan);
+      Scan scan = new Scan();
+      FilterList filterList1 = new FilterList(filters);
+      scan.setFilter(filterList1);
       //scan.setStartRow(startScan);
       //scan.setStopRow(endScan);
 
